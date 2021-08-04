6f9a7aecd0749e8eb1dd38455f636de472ae5a9b
satu-n <49983831+satu-n@users.noreply.github.com>
2021-08-05 08:27:13 +0900

    done 05 primitive_types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,9 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = std::iter::repeat(0).take(200).collect::<Vec<i32>>();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
