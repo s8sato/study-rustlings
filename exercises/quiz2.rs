2809ccf8be60c5f9c5d94581eff02f838b465821
satu-n <49983831+satu-n@users.noreply.github.com>
2021-08-05 14:39:36 +0900

    done 10 strings

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -7,7 +7,6 @@
 // you think each value is. That is, add either `string_slice` or `string`
 // before the parentheses on each line. If you're right, it will compile!
 
-// I AM NOT DONE
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -17,14 +16,31 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
+
+// fn string_like(arg: impl ToString + std::fmt::Display) {
+//     println!("{}", arg);
+// }
+
+// fn main() {
+//     string_like("blue");
+//     string_like("red".to_string());
+//     string_like(String::from("hi"));
+//     string_like("rust is fun!".to_owned());
+//     // string_like("nice weather".into());
+//     string_like(format!("Interpolation {}", "Station"));
+//     string_like(&String::from("abc")[0..1]);
+//     string_like("  hello there ".trim());
+//     string_like("Happy Monday!".to_string().replace("Mon", "Tues"));
+//     string_like("mY sHiFt KeY iS sTiCkY".to_lowercase());
+// }
